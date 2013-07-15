3fabd5fd217d0d17219f398395d7eac161cc4e89
eguciar <egucciar@gmail.com>
2013-07-15 17:51:50 -0400

    fixed directory error

--- scalejs.autocomplete-textext/tools/uninstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿param($installPath, $toolsPath, $package, $project)
-
-$project |
-	Remove-Paths 'scalejs.autocomplete-textext' |
-	Remove-Shims 'textext' |
-	Remove-ScalejsExtension 'scalejs.autocomplete-textext' |
-	Out-Null
